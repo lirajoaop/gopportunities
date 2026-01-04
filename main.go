e7fe4f7ba54bdf41cb62b16aa34c3e5eebd57e45
João Pedro Lira <joaopedroababa132@gmail.com>
2026-01-04 00:51:52 -0300

    feat: add opening schema package

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/lirajoaop/gopportunities/router"
+import (
+	"github.com/lirajoaop/gopportunities/config"
+	"github.com/lirajoaop/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
