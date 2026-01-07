8a7416a4ca1177108c54e8b3dbe67d64bb32b410
João Pedro Lira <joaopedroababa132@gmail.com>
2026-01-07 20:42:52 -0300

    feat: refactor CreateOpeningHandler to use Opening schema and improve response handling

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lirajoaop/gopportunities/schemas"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
@@ -17,9 +18,20 @@ func CreateOpeningHandler(ctx *gin.Context) {
 		return
 	}
 
-	if err := db.Create(&request).Error; err != nil {
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
 		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
 		return
 	}
 
+	sendSuccess(ctx, "create-opening", opening)
 }
--- handler/response.go
+++ handler/response.go
@@ -1,10 +1,23 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func sendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-Type", "application/json")
 	ctx.JSON(code, gin.H{
 		"message":   msg,
 		"errorCode": code,
+	})
+}
+
+func sendSuccess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(200, gin.H{
+		"message": fmt.Sprintf("operation %s successful", op),
+		"data":    data,
 	})
 }
